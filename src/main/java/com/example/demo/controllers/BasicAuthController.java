daadb44f02f91645cbbb701a74ed7987d8217377
giapham <luatgia.pham@mafc.com.vn>
2024-10-30 11:20:34 +0700

    making get create order api

--- src/main/java/com/example/demo/controllers/BasicAuthController.java
+++ src/main/java/com/example/demo/controllers/BasicAuthController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/basic")
 public class BasicAuthController {
 
-    @GetMapping("/test")
-    public ResponseEntity<String> secureEndpointBasic() {
-        return ResponseEntity.ok("Basic Auth API accessed!");    }
+    @GetMapping("/create-order/{id}")
+    public ResponseEntity<String> secureEndpointBasic(@PathVariable ("id") Integer id) throws Exception {
+
+        if(id == 0){
+            throw new Exception("This is a simulated runtime exception.");
+        }
+        return ResponseEntity.ok("Tạo Order Thành Công!");
+    }
 }
