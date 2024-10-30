88d549eb82279d851719b45f21a9e24593e77597
giapham <luatgia.pham@mafc.com.vn>
2024-10-30 10:27:07 +0700

    making get create product api

--- src/main/java/com/example/demo/controllers/BasicAuthController.java
+++ src/main/java/com/example/demo/controllers/BasicAuthController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/basic")
 public class BasicAuthController {
 
-    @GetMapping("/test")
-    public ResponseEntity<String> secureEndpointBasic() {
-        return ResponseEntity.ok("Basic Auth API accessed!");    }
+    @GetMapping("/create-products/{id}")
+    public ResponseEntity<String> secureEndpointBasic(@PathVariable ("id") Integer id) {
+        if(id > 1){
+            return ResponseEntity.ok("Tạo Product Không Thành Công, Vì Id Lớn Hơn 1!");
+        }
+        return ResponseEntity.ok("Tạo Product Thành Công!");
+    }
 }
