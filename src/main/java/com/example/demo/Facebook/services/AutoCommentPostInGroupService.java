5b39940ea509e69530d63d965dec8036901cf724
giapham <giapham121994@gmail.com>
2024-11-22 13:59:58 +0700

    edit for complete

--- src/main/java/com/example/demo/Facebook/services/AutoCommentPostInGroupService.java
+++ src/main/java/com/example/demo/Facebook/services/AutoCommentPostInGroupService.java
@@ -1,5 +1,6 @@
 package com.example.demo.Facebook.services;
 
+import com.example.demo.Facebook.commonFunc.ConfigCommonFunc;
 import com.example.demo.Facebook.models.AutoCommentPostGroup;
 import com.example.demo.Facebook.models.AutoPostGroup;
 import org.openqa.selenium.*;
@@ -7,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -20,40 +22,44 @@ import java.util.regex.Pattern;
 
 @Service
 public class AutoCommentPostInGroupService {
+
+    @Autowired
+    ConfigCommonFunc configCommonFunc;
+
     public ResponseEntity<String> autoCommentPost(AutoCommentPostGroup autoPostGroup) throws InterruptedException {
-        if(autoPostGroup.getTypeComp().toUpperCase().equals("MAC")){
-            System.setProperty("webdriver.chrome.driver", "/Users/giapham/Documents/chromedriver-mac-x64/chromedriver");
-        }else{
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
-        }
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.setExperimentalOption("detach", false);
-        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
-
-        WebDriver driver = new ChromeDriver(options);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-        // Add cookies for login
-        driver.get("https://www.facebook.com");
-        List<Cookie> cookies = new ArrayList<>();
-
-        cookies.add(new Cookie("c_user", "61568239606429"));//1
-        cookies.add(new Cookie("datr", "mwg0Z9THOO1-yWEpQbBW07Sx"));//2
-        cookies.add(new Cookie("i_user", "100066835222220"));
-        cookies.add(new Cookie("locale", "en_US"));
-        cookies.add(new Cookie("ps_l", "1"));
-        cookies.add(new Cookie("ps_n", "1"));
-        cookies.add(new Cookie("sb", "mwg0Z16z_I75ZUIAXFwsTihu"));//4
-        cookies.add(new Cookie("wd", "872x75"));
-        cookies.add(new Cookie("fr", "1K0gQHZbuaUpoSS03.AWV5LR_JDEgmfee-hSvtEMAEXEw.BnPZyS..AAA.0.0.BnPe1j.AWXlZtq1FII"));//3
-        cookies.add(new Cookie("xs", "14%3APDjZjuM9Bp0zjw%3A2%3A1732111650%3A-1%3A-1"));
-
-        // Add necessary cookies here
-        for (Cookie cookie : cookies) {
-            driver.manage().addCookie(cookie);
-        }
-
+//        if(autoPostGroup.getTypeComp().toUpperCase().equals("MAC")){
+//            System.setProperty("webdriver.chrome.driver", "/Users/giapham/Documents/chromedriver-mac-x64/chromedriver");
+//        }else{
+//            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
+//        }
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-notifications");
+//        options.setExperimentalOption("detach", false);
+//        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+//
+//        WebDriver driver = new ChromeDriver(options);
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+//
+//        // Add cookies for login
+//        driver.get("https://www.facebook.com");
+//        List<Cookie> cookies = new ArrayList<>();
+//
+//        cookies.add(new Cookie("c_user", "61568239606429"));//1
+//        cookies.add(new Cookie("datr", "mwg0Z9THOO1-yWEpQbBW07Sx"));//2
+//        cookies.add(new Cookie("i_user", "100066835222220"));
+//        cookies.add(new Cookie("locale", "en_US"));
+//        cookies.add(new Cookie("ps_l", "1"));
+//        cookies.add(new Cookie("ps_n", "1"));
+//        cookies.add(new Cookie("sb", "mwg0Z16z_I75ZUIAXFwsTihu"));//4
+//        cookies.add(new Cookie("wd", "872x75"));
+//        cookies.add(new Cookie("fr", "1K0gQHZbuaUpoSS03.AWV5LR_JDEgmfee-hSvtEMAEXEw.BnPZyS..AAA.0.0.BnPe1j.AWXlZtq1FII"));//3
+//        cookies.add(new Cookie("xs", "14%3APDjZjuM9Bp0zjw%3A2%3A1732111650%3A-1%3A-1"));
+//
+//        // Add necessary cookies here
+//        for (Cookie cookie : cookies) {
+//            driver.manage().addCookie(cookie);
+//        }
+        WebDriver driver = configCommonFunc.loginByCookie(autoPostGroup.getTypeComp());
         // Navigate to the post page after adding cookies
         driver.navigate().to("https://facebook.com/groups/"+ autoPostGroup.getGroupId());
         for(int i =0; i<10; i++){
@@ -78,7 +84,6 @@ public class AutoCommentPostInGroupService {
             if (url != null && !url.isEmpty() && url.contains("pcb")) {
                 // Define a pattern to match numbers after 'pcb.'
                 Pattern pattern = Pattern.compile("pcb.(\\d+)");
-
                 // Loop through each link and extract the number after 'pcb.'
                 Matcher matcher = pattern.matcher(url);
                 if (matcher.find()) {
@@ -94,16 +99,20 @@ public class AutoCommentPostInGroupService {
                 WebElement postBox = driver.findElement(By.xpath("//div[@aria-label='Answer as Ốp Lưng Điện Thoại Iphone - Samsung']"));
                 postBox.click();
                 postBox.sendKeys(autoPostGroup.getContent() + Keys.ENTER);
-                Thread.sleep(5000); // Wait for the next set of groups to load
-
+                Thread.sleep(3000); // Wait for the next set of groups to load
             }catch (Exception e){
-                WebElement postBox = driver.findElement(By.xpath("//div[@aria-label='Comment as Ốp Lưng Điện Thoại Iphone - Samsung']"));
-                postBox.click();
-                postBox.sendKeys(autoPostGroup.getContent() + Keys.ENTER);
-                Thread.sleep(5000); // Wait for the next set of groups to load
+                try {
+                    WebElement postBox = driver.findElement(By.xpath("//div[@aria-label='Comment as Ốp Lưng Điện Thoại Iphone - Samsung']"));
+                    postBox.click();
+                    postBox.sendKeys(autoPostGroup.getContent() + Keys.ENTER);
+                    Thread.sleep(3000); // Wait for the next set of groups to load
+                }catch (Exception ex){
+                    System.out.println(ex);
+                    return ResponseEntity.ok("Failed for comment posts in group. Please check element.");
+                }
             }
 
         }
-        return ResponseEntity.ok("dá");
+        return ResponseEntity.ok("Comment for posts in group success. Total posts for comment: " + uniqueNumbers.size());
     }
 }
--- src/main/java/com/example/demo/Facebook/services/AutoPostInGroupService.java
+++ src/main/java/com/example/demo/Facebook/services/AutoPostInGroupService.java
@@ -1,11 +1,13 @@
 package com.example.demo.Facebook.services;
 
+import com.example.demo.Facebook.commonFunc.ConfigCommonFunc;
 import com.example.demo.Facebook.models.AutoPostGroup;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,40 +17,45 @@ import java.util.List;
 
 @Service
 public class AutoPostInGroupService {
+
+    @Autowired
+    ConfigCommonFunc configCommonFunc;
+
     public ResponseEntity<String> autoCommentPost(AutoPostGroup autoPostGroup) throws InterruptedException {
-        if(autoPostGroup.getTypeComp().toUpperCase().equals("MAC")){
-            System.setProperty("webdriver.chrome.driver", "/Users/giapham/Documents/chromedriver-mac-x64/chromedriver");
-        }else{
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
-        }
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.setExperimentalOption("detach", false);
-        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
-
-        WebDriver driver = new ChromeDriver(options);
+//        if(autoPostGroup.getTypeComp().toUpperCase().equals("MAC")){
+//            System.setProperty("webdriver.chrome.driver", "/Users/giapham/Documents/chromedriver-mac-x64/chromedriver");
+//        }else{
+//            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
+//        }
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-notifications");
+//        options.setExperimentalOption("detach", false);
+//        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+//
+//        WebDriver driver = new ChromeDriver(options);
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+//
+//        // Add cookies for login
+//        driver.get("https://www.facebook.com");
+//        List<Cookie> cookies = new ArrayList<>();
+//
+//        cookies.add(new Cookie("c_user", "61568239606429"));//1
+//        cookies.add(new Cookie("datr", "mwg0Z9THOO1-yWEpQbBW07Sx"));//2
+//        cookies.add(new Cookie("i_user", "100066835222220"));
+//        cookies.add(new Cookie("locale", "en_US"));
+//        cookies.add(new Cookie("ps_l", "1"));
+//        cookies.add(new Cookie("ps_n", "1"));
+//        cookies.add(new Cookie("sb", "mwg0Z16z_I75ZUIAXFwsTihu"));//4
+//        cookies.add(new Cookie("wd", "872x75"));
+//        cookies.add(new Cookie("fr", "11iZMNBDaCFFhGkbk.AWVzh9MCkJ2tlqbhvEhgFumKjE0.BnPWQZ..AAA.0.0.BnPZG3.AWXdi5ZHMLk"));//3
+//        cookies.add(new Cookie("xs", "11%3AWe5fJeHEAhBxyg%3A2%3A1731463337%3A-1%3A-1%3A%3AAcXGLjzU8I-4z_bOtDFNlTvRBaSJadBy_8UAZCUabTk"));
+//
+//        // Add necessary cookies here
+//        for (Cookie cookie : cookies) {
+//            driver.manage().addCookie(cookie);
+//        }
+        WebDriver driver = configCommonFunc.loginByCookie(autoPostGroup.getTypeComp());
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-        // Add cookies for login
-        driver.get("https://www.facebook.com");
-        List<Cookie> cookies = new ArrayList<>();
-
-        cookies.add(new Cookie("c_user", "61568239606429"));//1
-        cookies.add(new Cookie("datr", "mwg0Z9THOO1-yWEpQbBW07Sx"));//2
-        cookies.add(new Cookie("i_user", "100066835222220"));
-        cookies.add(new Cookie("locale", "en_US"));
-        cookies.add(new Cookie("ps_l", "1"));
-        cookies.add(new Cookie("ps_n", "1"));
-        cookies.add(new Cookie("sb", "mwg0Z16z_I75ZUIAXFwsTihu"));//4
-        cookies.add(new Cookie("wd", "872x75"));
-        cookies.add(new Cookie("fr", "11iZMNBDaCFFhGkbk.AWVzh9MCkJ2tlqbhvEhgFumKjE0.BnPWQZ..AAA.0.0.BnPZG3.AWXdi5ZHMLk"));//3
-        cookies.add(new Cookie("xs", "11%3AWe5fJeHEAhBxyg%3A2%3A1731463337%3A-1%3A-1%3A%3AAcXGLjzU8I-4z_bOtDFNlTvRBaSJadBy_8UAZCUabTk"));
-
-        // Add necessary cookies here
-        for (Cookie cookie : cookies) {
-            driver.manage().addCookie(cookie);
-        }
-
         // Navigate to the post page after adding cookies
         driver.navigate().to("https://facebook.com/groups/"+ autoPostGroup.getGroupId());
 
@@ -80,15 +87,14 @@ public class AutoPostInGroupService {
             Thread.sleep(5000); // Wait for the next set of groups to load
 
             //Click Post Button
-//            WebElement clickPost = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@aria-label='Post']")));
-//            clickPost.click();
-//            Thread.sleep(5000); // Chờ ảnh tải lên
-        } catch(Exception v){
-            Thread.sleep(5000); // Wait for the next set of groups to load
-            System.out.println(v);
+            WebElement clickPost = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@aria-label='Post']")));
+            clickPost.click();
+            Thread.sleep(3000);
+        } catch(Exception e){
+            System.out.println(e);
+            return ResponseEntity.ok("Post to group failed. Please check element.");
         } finally {
-            // Đóng trình duyệt
-//            driver.quit();
+            driver.quit();
         }
         return ResponseEntity.ok("Post to group success.");
     }
